5c67d7348a709866e4bc2cdc3c10c4c439eed511
dan850 <dan06982@gmail.com>
2022-11-18 20:16:46 -0600

    update

--- app.js
+++ app.js
@@ -7,34 +7,35 @@ const openFace2 = document.querySelector('.open2');
 
 
 // Add event listener
+// Add event listener
 closedFace.addEventListener('click', () => {
   if (openFace.classList.contains('open')) {
     openFace.classList.add('active');
     closedFace.classList.remove('active');
   }
+
 });
 
 openFace.addEventListener('click', () => {
   if (closedFace.classList.contains('closed')) {
-    openFace2.classList.add('active2');
-
     closedFace.classList.add('active');
     openFace.classList.remove('active');
   }
-});       
+});
 
-
-
+  
 
 closedFace2.addEventListener('click', () => {
     if (openFace2.classList.contains('open2')) {
+      openFace2.classList.add('active2');
       closedFace2.classList.remove('active2');
     }
+
   });
-  
   openFace2.addEventListener('click', () => {
     if (closedFace2.classList.contains('closed2')) {
       closedFace2.classList.add('active2');
       openFace2.classList.remove('active2');
     }
-  });       
+
+}); 
